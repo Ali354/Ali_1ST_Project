8f71136c2085e699a420df94439814cbbba7d028
Ali Mahfood <mahfoodali795@gmail.com>
2025-03-30 16:41:21 +0300

    2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { firstItem } from '../shared/models/firstItem';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -8,5 +8,10 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  items = [
+    new firstItem('Learn Angular1'),
+    new firstItem('Learn Angular2', true),
+    new firstItem('Learn Angular3')
+  ];
   title = 'Ali_1ST_Project';
 }
---
+++ src/shared/models/firstItem.ts
@@ -0,0 +1,5 @@
+export class firstItem{
+    constructor(public text: string, public isCompleted: boolean = false){
+        
+    }
+}
